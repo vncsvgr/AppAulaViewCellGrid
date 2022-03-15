77c8829659272be13ee996ccce4e57201c7416ec
vncsvgr <vinicius.gabriel83@yahoo.com>
2022-03-14 22:21:31 -0300

    Adicionar arquivos de projeto.

---
+++ AppGrid/AppGrid/Aluno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGrid
+{
+    internal class Aluno
+    {
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+        public string Cidade { get; set; }
+    }
+}
---
+++ AppGrid/AppGrid/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppGrid
+{
+    public partial class MainPage : ContentPage
+    {
+        List<Aluno> aluno = new List<Aluno>
+        {
+            new Aluno { Nome="João", Curso="Desenvolvimento", Cidade="Jau"},
+            new Aluno { Nome="José", Curso="Administração", Cidade="Bariri"},
+            new Aluno { Nome="Paulo", Curso="Mecânica", Cidade="Bocaina"},
+            new Aluno { Nome="Antonio", Curso="Edificações", Cidade="Bauru"}
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            lstAlunos.ItemsSource = aluno;
+        }
+
+        async void lstAlunos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var a = e.SelectedItem as Aluno;
+
+            var resp = await DisplayAlert("Exclusão", "Deseja excluir o aluno? " + a.Nome, "Sim", "Não");
+
+            if (resp)
+            {
+                var item = aluno.Find(x => x.Nome == a.Nome);
+                aluno.Remove(item);
+                lstAlunos.ItemsSource = null;
+                lstAlunos.ItemsSource = aluno;
+            }
+        }
+    }
+}
